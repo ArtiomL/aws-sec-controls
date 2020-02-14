ef475dc3945a29cedea588c40607c778e36a305e
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2020-02-14 12:00:28 +0200

    Input Variables

--- variables.tf
+++ variables.tf
@@ -5,6 +5,29 @@ variable "aws_region" {
   default     = "eu-west-1"
 }
 
+variable "regions" {
+  description = "A list of regions to enable security controls in"
+
+  default = [
+    "us-east-1",
+    "us-east-2",
+    "us-west-1",
+    "us-west-2",
+    "ap-south-1",
+    "ap-northeast-2",
+    "ap-southeast-1",
+    "ap-southeast-2",
+    "ap-northeast-1",
+    "ca-central-1",
+    "eu-central-1",
+    "eu-west-1",
+    "eu-west-2",
+    "eu-west-3",
+    "eu-north-1",
+    "sa-east-1",
+  ]
+}
+
 # S3
 variable "force_destroy" {
   description = "All objects should be deleted from the bucket so that the bucket can be destroyed without error"
